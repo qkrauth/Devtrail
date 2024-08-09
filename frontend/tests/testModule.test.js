cc0198d487648655643ef17bf2cb7fcf45871420
Quinten Krauth <quintenkrauth19@gmail.com>
2024-08-09 17:26:03 -0400

    tests failing

--- frontend/tests/testModule.test.js
+++ frontend/tests/testModule.test.js
@@ -1,8 +1,5 @@
 const dummyTestSum = require('./testModule');
 
-import { render, screen, } from '@testing-library/react';
-import HomeScreen from '../src/screens/HomeScreen';
-
 test("adds 2 + 2 and equals 4", () => {
     expect(dummyTestSum(2, 2)).toBe(4)
 });
