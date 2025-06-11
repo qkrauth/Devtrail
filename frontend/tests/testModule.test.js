e61440a1cb213ed1991220c3dcbc87b7c08494cf
Quinten Krauth <quintenkrauth19@gmail.com>
2025-06-11 16:52:23 -0400

    comments for tests

--- frontend/tests/testModule.test.js
+++ frontend/tests/testModule.test.js
@@ -3,10 +3,12 @@ import { render, screen } from '@testing-library/react';
 import Intro from '../src/components/Intro';
 import dummyTestSum from '../tests/testModule';
 
+// initial dummy test
 test("adds 2 + 2 and equals 4", () => {
     expect(dummyTestSum(2, 2)).toBe(4);
 });
 
+// render test, to see if a component loads when the app starts
 test("renders the Intro component on the main screen", () => {
     render(<Intro />);
 
